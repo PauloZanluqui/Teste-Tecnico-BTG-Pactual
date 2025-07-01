6bc2c3a1f346c570117a02edb6a7aed65e766d9b
Paulo Zanluqui <paulozanluqui13@gmail.com>
2025-06-30 23:51:10 -0300

    feat: Implementação de environments

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
-    private baseUrl = 'http://localhost:5201';
+    private baseUrl = environment.apiUrl;
 
     constructor(private http: HttpClient) { }
 
